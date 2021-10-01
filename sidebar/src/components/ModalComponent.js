f9ab64f78714a5bbe92daf9e2eea9a1d86a09737
Elijah Edun <edunelijah18@gmail.com>
2021-10-01 14:20:02 +0100

    Modified the sub-modal on the sidebar

--- sidebar/src/components/ModalComponent.js
+++ sidebar/src/components/ModalComponent.js
@@ -1,29 +1,58 @@
 import React, { useState } from 'react'
 import styles from '../styles/ModalComponentStyles.module.css'
+import arrow from '../assets/Shape.svg'
 
 const ModalComponent = ({ isOpen }) => {
+  // const switchModal = () => {
+
+  // }
   return (
     <section className={`${isOpen ? styles.open : styles.topbarModal}`}>
       <div className={styles.sectionOne}>The BrandHub</div>
 
-      <div className={styles.sectionOne}>
-        Invite people to HNGi8 <br />
-        Create a new Workspace
+      <div className="my-2">
+        <div className="p-1">Invite people to HNGi8</div>
+
+        <div className="p-1">Create a new Workspace</div>
       </div>
 
-      <div className={styles.sectionOne}>
-        Prefrence <br />
-        Customize HNGi8
+      <div className="my-2">
+        <div className="p-1">Prefrence</div>
+
+        <div className="p-1">
+          Customize HNGi8{' '}
+          <span className="float-end">
+            <img src={arrow} />
+          </span>
+        </div>
       </div>
 
-      <div className={styles.sectionOne}>Tools</div>
-      <div className={styles.sectionOne}>Sign Out</div>
-      <div
-        className={` d-flex flex-column align-items-start ${styles.sectionOne}`}
-      >
-        <span> Add workspace </span>
-        <span>Switch workspace</span>
-        <span>Open the Zuri Chat App </span>
+      <div className="my-2 p-1">
+        Tools{' '}
+        <span className="float-end">
+          <img src={arrow} />
+        </span>
+      </div>
+
+      <div className="my-2 p-1">Sign Out</div>
+
+      <div className="my-2">
+        <div className="p-1">
+          Add workspace{' '}
+          <span className="float-end">
+            <img src={arrow} />
+          </span>
+        </div>
+        <div
+          className="p-1"
+          // onClick={switchModal}
+        >
+          Switch workspace{' '}
+          <span className="float-end">
+            <img src={arrow} />
+          </span>
+        </div>
+        <div className="p-1">Open the Zuri Chat App</div>
       </div>
     </section>
   )
